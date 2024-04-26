7866d860698683a3bf0f16de20fb25a4a24cf7be
Erickkirui <erickkirui653@gmail.com>
2024-04-26 16:11:04 +0300

    added the home search section

--- app/page.tsx
+++ app/page.tsx
@@ -2,26 +2,32 @@ import HomeCategories from "./Components/HomeCategories";
 import HomeFaeturedlist from "./Components/HomeFaeturedlist";
 import HomeHeader from "./Components/HomeHeader";
 import HomeRegister from "./Components/HomeRegister";
+import HomeSearch from "./Components/HomeSearch";
+
+
 export default function Home() {
   return (
     <div>
-      <div className= "mb-14">
-         <HomeHeader  />
-      </div>
-      
-      <div className="mb-14">
-        <HomeCategories />
-      </div>
+        <div className= "mb-14">
+          <HomeHeader  />
+        </div>
+        
+        <div className="mb-14">
+          <HomeCategories />
+        </div>
 
-      <div>
-        <HomeFaeturedlist />
-      </div>
-      <div>
-        <HomeRegister />
-      </div>
-      
-      
+        <div>
+          <HomeFaeturedlist />
+        </div>
 
+        {/* <div>
+          <HomeSearch />
+        </div> */}
+
+        <div>
+          <HomeRegister />
+        </div>
+      
     </div>
    
   );
