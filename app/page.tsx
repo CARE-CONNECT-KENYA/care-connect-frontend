3a6cf7818ac287bcf469faaa5c76f377c6c48e3b
Erickkirui <erickkirui653@gmail.com>
2024-04-29 15:44:14 +0300

    added the search section

--- app/page.tsx
+++ app/page.tsx
@@ -20,9 +20,9 @@ export default function Home() {
           <HomeFaeturedlist />
         </div>
 
-        {/* <div>
+        <div>
           <HomeSearch />
-        </div> */}
+        </div>
 
         <div>
           <HomeRegister />
