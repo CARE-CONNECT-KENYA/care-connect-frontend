9fa08e59c67c87c626d35bac460e53844355af76
erickkirui <erickkirui653@gmail.com>
2024-06-02 10:49:51 +0300

    layout for listing pages

--- app/Doctors/page.tsx
+++ app/Doctors/page.tsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import ProvidersCard from '../Components/ProvidersCard'
+import styles from '../Styles/listings.module.css'
 
 function DoctorsList() {
   return (
-    <div>
-        <ProvidersCard />
-      
+    <div className={styles.listingsContainer}>
+        <div>
+          <ProvidersCard />
+        </div>
+        <div>
+          <div>
+            <h2>
+              Side bar
+            </h2>
+          </div>
+        </div>  
     </div>
   )
 }
--- app/users/login/page.tsx
+++ app/users/login/page.tsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import RootLayout from '@/app/layout'
 
-
-
 function Userlogin() {
   return (
  
