7fd02a54b2a8b7f27a5ae56c22f92516259c0eb5
Erickkirui <erickkirui653@gmail.com>
2024-08-26 12:15:12 +0300

    adde collapse sidebar

--- app/(without-layout)/superadmin/SideBar.tsx
+++ app/(without-layout)/superadmin/SideBar.tsx
@@ -1,19 +1,36 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react';
 import styles from '../../Styles/dashbordlayout.module.css';
-
+import { FaBars, FaTimes, FaHome, FaUser, FaUserMd } from 'react-icons/fa'; // Import icons
 
 function SideBar() {
+  const [isCollapsed, setIsCollapsed] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsCollapsed(!isCollapsed);
+  };
+
   return (
-    <>
-    <div className={styles.DashbordSidebar}>
-          <ul>
-            <li>Home</li>
-            <li>Users</li>
-            <li>Providers</li>
-          </ul>
-        </div>
-    </>
-  )
+    <div className={`${styles.DashbordSidebar} ${isCollapsed ? styles.collapsed : ''}`}>
+      <div className={styles.sidebarToggle} onClick={toggleSidebar}>
+        {isCollapsed ? <FaBars /> : <FaTimes />}
+      </div>
+      <ul>
+        <li>
+          <FaHome className={styles.sidebarIcon} />
+          {!isCollapsed && <span>Home</span>}
+        </li>
+        <li>
+          <FaUser className={styles.sidebarIcon} />
+          {!isCollapsed && <span>Users</span>}
+        </li>
+        <li>
+          <FaUserMd className={styles.sidebarIcon} />
+          {!isCollapsed && <span>Providers</span>}
+        </li>
+      </ul>
+    </div>
+  );
 }
 
-export default SideBar
+export default SideBar;
